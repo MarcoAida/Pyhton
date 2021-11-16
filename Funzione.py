cdaee7da13ca5bb5c2a8bf89bc237e018f93f0eb
Marco Casalini <91371324+MarcoAida@users.noreply.github.com>
2021-11-16 21:01:54 +0000

    defSumList

--- Funzione.py
+++ Funzione.py
@@ -1,9 +1,16 @@
-my_list = [1, 2, 3, 4, 5]
-def sum_list(my_list):
-    x = 0
-    
-    for i in list(len(my_list)):
-        
-        x = x + my_list
+#Scrivete una funzione che sommi
+#tutti gli elementi di una lista
 
-print(sum_list(my_list))
+from random import seed
+from random import randint
+
+#my_list = [16, 2, 7, 31, 5]
+my_list = [randint(0, 100), randint(0, 50), randint(0, 200)]
+
+def sum_list(my_list):
+    sum = 0
+    for i in range(len(my_list)):
+        sum += my_list[i]
+    return sum
+
+print("Somma: {}" .format(sum_list(my_list)))
