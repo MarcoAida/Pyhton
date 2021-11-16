c060f9c82f76cf48dd05e6761de29d9c672d9417
Marco Casalini <91371324+MarcoAida@users.noreply.github.com>
2021-11-16 16:59:08 +0000

    def

---
+++ Funzione.py
@@ -0,0 +1,9 @@
+my_list = [1, 2, 3, 4, 5]
+def sum_list(my_list):
+    x = 0
+    
+    for i in list(len(my_list)):
+        
+        x = x + my_list
+
+print(sum_list(my_list))
--- Hello.py
+++ Hello.py
@@ -1,1 +1,9 @@
 printf("Hello World!")
+
+for i in range(10):
+  print(i)
+    #print(i)
+
+my_list = ["marco", "irene", "paolo"]
+for i in range(len(my_list)):
+    print(my_list[i])
