121ff1fa6f8bdf04f66dd6404e1f09f755741300
Marco Casalini <91371324+MarcoAida@users.noreply.github.com>
2021-11-19 18:38:52 +0000

    test done

--- Esercitazione/test.py
+++ Esercitazione/test.py
@@ -7,17 +7,20 @@ def my_fun(values):
         sum += values[i]
     return sum
 
+j = 0       #controllo    primi 4
+
 for line in my_file:
     elements = line.split(',')
     
+    #controllo    primi 4
+    if(j > 3):
+        break;
+
     if elements [0] != 'Date':
         date  = elements [0]
         value = elements [1]
 
         values.append(float(value))
-        
-        print(my_fun(values).readline())
-        print(my_fun(values).readline())
-        print(my_fun(values).readline())
+        j = j + 1         #controllo    primi 4
 
 print("Somma: {}" .format(my_fun(values )))
