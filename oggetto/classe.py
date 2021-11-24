01c895089b26bf0dfc6ed0bac76809f8cdf98ce8
Marco Casalini <91371324+MarcoAida@users.noreply.github.com>
2021-11-24 12:41:42 +0000

    corretto

--- oggetto/classe.py
+++ oggetto/classe.py
@@ -5,17 +5,17 @@ class CSVFile():
 
     def get_data(self):
         my_file = open(self.name, 'r')
+        my_list = []
 
         for line in my_file:
             element = line.split(',')
 
             if element [0] != 'Date':
-                my_file.append(element)
+                my_list.append(element)
 
-            return(element)
+        return(my_list)
 
 shampooFile = CSVFile('shampoo_sales_.txt')
 shampoo = shampooFile.get_data()
 
-for line in range(40):
-    print(shampoo)
+print(shampoo)
