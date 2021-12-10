8901c7bdd561775596f6bcbdb5264d0df0ae73d7
Marco Casalini <91371324+MarcoAida@users.noreply.github.com>
2021-12-10 21:06:39 +0000

    model2

--- lezione8/model.py
+++ lezione8/model.py
@@ -1,3 +1,50 @@
+values = []
+my_file = open('sales.txt', 'r')
+
+def my_fun(values):
+    sum = 0;
+    for i in range(len(values)):
+        sum += values[i]
+    return sum
+
+for line in my_file:
+    elements = line.split(',')
+    
+    if elements [0] != 'Date':
+        date  = elements [0]
+        value = elements [1]
+
+        values.append(float(value))
+        
+        my_fun(values)
+
+print("Somma: {}" .format (my_fun(values)) )
+
+class NumericalCSVFile():
+
+    def __init__(self, name):
+        self.name = name
+        my_file = open(self.name, 'r')
+        
+    def get_data(self):
+        my_file = open(self.name, 'r')
+        my_list = []
+
+        for line in my_file:
+            element = line.split(',')
+            element[1] = element[1].strip()
+
+            if element [0] != 'Date':
+                value = element [1]
+                my_list.append(element)
+
+        return(my_list)
+
+shampooFile = NumericalCSVFile('sales.txt')
+sales = shampooFile.get_data()
+
+print(sales)
+
 class Model():
     def fit(self, data):
         # Fit non implementanto nella classe base
@@ -8,15 +55,12 @@ def predict(self, data):
         raise NotImplementedError('Metodo non implementato')
 
 class IncrementModel(Model):
+
     def predict(self, data):
         prev_value = None
-        lenght = len(data)
-        l = []
-        im = 0
+        
         if(data != None and lenght > 1): # Logica per la predizione
             for i in l:
-                l[i+1] = l[3] + t[i-1]
-                im = t
                 
-        prediction = im/n
+            prediction = im/n
         return prediction
