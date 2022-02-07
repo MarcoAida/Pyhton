fea8f435949938fec4d81ab6e63c807891d11b7b
Marco Casalini <91371324+MarcoAida@users.noreply.github.com>
2022-02-07 04:49:24 +0000

    calcolo mensile

--- test.py
+++ test.py
@@ -5,25 +5,37 @@ def compute_avg_monthly_difference(time_series, first_year, last_year):
 
     myavglist = []
     n = 0
+    l = 0
+    s = 0
     
     for line in my_data:
         
-        element = line.split(',')     #passenger  1949-01 element[0]   ,   112 element[1]
+        element = line.split(',')     #passengers  1949-01 element[0]   ,   112 element[1]
         element[1] = element[1].strip()
 
         elements = line.split('-')    #year   1949 elements[0]   -   01,112 elements[1]
-        #elements[1] = elements[1].strip()
 
         if element[0] != 'date' and (int(elements[0]) >= first_year and int(elements[0]) <= last_year):
             
-            my_list.append(element[1])
-            
-            #myavglist.append(element[1])
-            
-            if int(elements[0]) <= last_year:
-            
-                n = ( (int(element[1])) + int(element[1]) ) / 2
-                myavglist.append(n)
+            my_list.append(element[1])  #passengers
+               
+            for i in range(first_year, last_year):
+                    
+                if int(elements[0]) % 2 == 0:
+
+                    for m in range(0, 11):
+
+                        l = ( int(element[1]) )
+
+                if int(elements[0]) % 2 == 1:
+                    
+                    for mm in range(0, 11):
+                    
+                        n = ( int(element[1]) )
+                 
+            s = (l - n) / 2
+
+            myavglist.append(s)
 
             #myavglist.append(element[1])
                              
@@ -68,3 +80,5 @@ x = compute_avg_monthly_difference(time_series, 1949, 1950)
 print(x)
 #print(y)
 #print(z)
+
+print('\n')
