2e6c7ed76f9769059c6d35b2ea16b2568827f4f8
Marco Casalini <91371324+MarcoAida@users.noreply.github.com>
2022-02-08 12:22:43 +0000

    test diversi anni

--- test.py
+++ test.py
@@ -31,15 +31,100 @@ def compute_avg_monthly_difference(time_series, first_year, last_year):
         raise ExamException('As number last_year must be bigger than first_year')
 
     myavglist = []
-    n = 0
-    l = 0
-    s = 0
+
     diff = (int(last_year) - int(first_year) ) + 1
     #print(diff)
-    
+
+    y1 = []
+    y2 = []
+    y3 = []
+    y4 = []
+    y5 = []
+    y6 = []
+    y7 = []
+    y8 = []
+    y9 = []
+    y10 = []
+    y11 = []
+    y12 = []
+
     for line in my_data:
         
-        element = line.split(',')     #passengers  1949-01 element[0]   ,   112 element[1]
+        element = line.split(',')
+        element[1] = element[1].strip()
+
+        elements = line.split('-')
+
+        if element[0] != 'date':
+        
+            if elements[0] == '1949':
+                y1.append(element[1])
+            
+            if elements[0] == '1950':
+                y2.append(element[1])
+
+            if elements[0] == '1951':
+                y3.append(element[1])
+
+            if elements[0] == '1952':
+                y4.append(element[1])
+
+            if elements[0] == '1953':
+                y5.append(element[1])
+                
+            if elements[0] == '1954':
+                y6.append(element[1])
+                
+            if elements[0] == '1955':
+                y7.append(element[1])
+
+            if elements[0] == '1956':
+                y8.append(element[1])
+
+            if elements[0] == '1957':
+                y9.append(element[1])
+
+            if elements[0] == '1958':
+                y10.append(element[1])
+
+            if elements[0] == '1959':
+                y11.append(element[1])
+
+            if elements[0] == '1960':
+                y12.append(element[1])
+
+    #print("1949: {}" .format(y1))
+    #print("1950: {}" .format(y2))
+    #print("1951: {}" .format(y3))
+    #print("1952: {}" .format(y4))
+    #print("1953: {}" .format(y5))
+    #print("1954: {}" .format(y6))
+    #print("1955: {}" .format(y7))
+    #print("1956: {}" .format(y8))
+    #print("1957: {}" .format(y9))
+    #print("1958: {}" .format(y10))
+    #print("1959: {}" .format(y11))
+    #print("1960: {}" .format(y12))
+
+    s = 0
+    s1 = 0
+    s2 = 0
+    s3 = 0
+    s4 = 0
+    s5 = 0
+    s6 = 0
+    s7 = 0
+    s8 = 0
+    s9 = 0
+    s10 = 0
+    s11 = 0
+    
+
+    for line in my_data:
+
+        #passengers  1949-01 element[0]   ,   112 element[1]
+
+        element = line.split(',')     
         element[1] = element[1].strip()
 
         if element[1] == int:
@@ -48,7 +133,9 @@ def compute_avg_monthly_difference(time_series, first_year, last_year):
         if element[1] == None:
             raise ExamException('Negative or zero value provided')
 
-        elements = line.split('-')    #year   1949 elements[0]   -   01,112 elements[1]
+        #year   1949 elements[0]   -   01,112 elements[1]
+
+        elements = line.split('-') 
 
         if elements[0] == int:
             raise ExamException('Invalid type, only int supported. Got "{}"'.format(type(elements[0])))
@@ -56,25 +143,74 @@ def compute_avg_monthly_difference(time_series, first_year, last_year):
         if elements[0] == None:
             raise ExamException('Negative or zero value provided')
 
-        if element[0] != 'date' and ( elements[0] >= first_year and elements[0] <= last_year ):
+        if element[0] != 'date':
+        
+            if elements[0] == '1949':
+                y1.append(element[1])
             
-            my_list.append(element[1])  #passengers
+            if elements[0] == '1950':
+                y2.append(element[1])
 
-            for i in range(0, diff):
+            if elements[0] == '1951':
+                y3.append(element[1])
+
+            if elements[0] == '1952':
+                y4.append(element[1])
+
+            if elements[0] == '1953':
+                y5.append(element[1])
                 
-                if int(elements[0]) % 2 == 0:
+            if elements[0] == '1954':
+                y6.append(element[1])
+                
+            if elements[0] == '1955':
+                y7.append(element[1])
 
-                    l = int(element[1])
+            if elements[0] == '1956':
+                y8.append(element[1])
 
-                if int(elements[0]) % 2 == 1:
-                    
-                    n = int(element[1])
+            if elements[0] == '1957':
+                y9.append(element[1])
 
-                s = l - n
+            if elements[0] == '1958':
+                y10.append(element[1])
+
+            if elements[0] == '1959':
+                y11.append(element[1])
+
+            if elements[0] == '1960':
+                y12.append(element[1])
+
+        #if elements[0] >= first_year and elements[0] <= last_year:
             
-            myavglist.append(s)
+    for i in range(0, 12):
+            
+        s1 = (int(y2[i]) - int(y1[i]) )     #50-49
 
-            #myavglist.append(element[1])
+        s2 = (int(y3[i]) - int(y2[i]) )     #51-50
+
+        s3 = (int(y4[i]) - int(y3[i]) )     #52-51
+
+        s4 = (int(y5[i]) - int(y4[i]) )     #53-52
+
+        s5 = (int(y6[i]) - int(y5[i]) )     #54-53
+
+        s6 = (int(y7[i]) - int(y6[i]) )     #55-54
+
+        s7 = (int(y8[i]) - int(y7[i]) )     #56-55
+
+        s8 = (int(y9[i]) - int(y8[i]) )     #57-56
+
+        s9 = (int(y10[i]) - int(y9[i]) )    #58-57
+
+        s10 = (int(y11[i]) - int(y10[i]) )    #59-58
+
+        s11 = (int(y12[i]) - int(y11[i]) )    #60-59
+        
+        s = ( (s1 + s2) / 2 )
+
+        myavglist.append(s)
+
           
     return myavglist
 
@@ -113,12 +249,12 @@ time_series_file = CSVTimeSeriesFile(name = 'testdata.csv')
 
 time_series = time_series_file.get_data()
 
-print(time_series)
+#print(time_series)
 
 #print("{}" .format(compute_avg_monthly_difference(time_series, 1949, 1950)) )
 print('\n')
 
-x = compute_avg_monthly_difference(time_series, "1949", "1950")
+x = compute_avg_monthly_difference(time_series, "1949", "1951")
 #y = compute_avg_monthly_difference(time_series, "1953", "1956")
 #z = compute_avg_monthly_difference(time_series, "1955", "1958")
 
